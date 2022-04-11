401e2ab4d1fc3f9e939d69cbecea2f7940782c84
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-04-10 19:51:16 -0600

    work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
